6033cba93991c87b9a55ab3abfc83ac9b6b2a201
Ryan John Vickramasinghe <rvickram@uwo.ca>
2020-11-30 19:45:31 -0500

    Added full input validation for account creation.

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { AccountService } from '../_services/account.service';
 
@@ -9,16 +10,64 @@ import { AccountService } from '../_services/account.service';
 })
 export class CreateAccountComponent implements OnInit {
 
-  nameVal: string = '';
-  emailVal: string = '';
-  passwordVal: string = '';
+  langs: string[] = [
+    'English',
+    'French',
+    'German',
+  ];
+  newAccForm: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
 
-  constructor(public accountService: AccountService) { }
+  createAccountForm;
 
-  ngOnInit(): void {
+  constructor(
+    public accountService: AccountService,
+  ) {
+      this.createFormControls();
+      this.createForm();
   }
 
-  createAccount() {
+  ngOnInit(): void {}
 
+  createFormControls() {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+    this.language = new FormControl('');
+  }
+
+  createForm() {
+    this.newAccForm = new FormGroup({
+      name: new FormGroup({
+        firstName: this.firstName,
+        lastName: this.lastName,
+      }),
+      email: this.email,
+      password: this.password,
+      language: this.language
+    });
+  }
+
+  onSubmit() {
+    if (this.newAccForm.valid) {
+      console.log(this.newAccForm);
+    } else {
+      console.log(this.newAccForm);
+    }
+  }
+
+  onClose() {
+    this.newAccForm.reset()
   }
 }
