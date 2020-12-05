7817fcf38e5760f474ebb6bcc0868c36f8ac462e
Ryan John Vickramasinghe <rvickram@uwo.ca>
2020-12-04 19:53:09 -0500

    Implemented skeleton for public schedules component.

--- src/app/pub-schedules/pub-schedules.component.ts
+++ src/app/pub-schedules/pub-schedules.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ScheduleManager } from '../_helpers/ScheduleManager';
+import { Schedule } from '../_models/Schedule';
+import { DataService } from '../_services/data.service';
 
 @Component({
   selector: 'app-pub-schedules',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PubSchedulesComponent implements OnInit {
 
-  constructor() { }
+  scheduleManager: ScheduleManager = new ScheduleManager();
+
+  constructor(
+    private dataService: DataService
+  ) { }
 
   ngOnInit(): void {
+   this.dataService.getPublicSchedules().subscribe(
+     pubSchedules => {
+      const newSchedList:Schedule[] = [];
+      for (let key in pubSchedules) {
+        const schedule: Schedule = this.scheduleManager.parseSchedule(pubSchedules[key]);
+        newSchedList.push(schedule);
+      }
+
+      this.scheduleManager.setSchedules(newSchedList);
+     }
+   )
   }
 
 }
